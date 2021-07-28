af35dd8209c040f667a4e6737951567f7cced32a
JuCSMartin <julianacmsantos@gmail.com>
2021-07-28 11:54:31 -0300

    Implementação dos componentes Contato, Sobre Nós e Experiências

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
 import { RodapeComponent } from './rodape/rodape.component';
+import { ExperienciasComponent } from './experiencias/experiencias.component';
+import { ContatoComponent } from './contato/contato.component';
+import { SobreNosComponent } from './sobre-nos/sobre-nos.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
-    RodapeComponent
+    RodapeComponent,
+    ExperienciasComponent,
+    ContatoComponent,
+    SobreNosComponent
   ],
   imports: [
     BrowserModule,
