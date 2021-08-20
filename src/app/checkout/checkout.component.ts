636b18d4e04748e99c890331dafdb822a6826004
Bruna de Freitas <84204465+Brynafreitas@users.noreply.github.com>
2021-08-20 20:13:43 -0300

    Estilização da página home e inserção da api paypal

--- src/app/checkout/checkout.component.ts
+++ src/app/checkout/checkout.component.ts
@@ -1,7 +1,11 @@
 import { ProdutoService } from './../service/produto.service';
 import { ActivatedRoute } from '@angular/router';
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { Produto } from '../model/Produto';
+
+
+
+declare var paypal: { Buttons: (arg0: { createOrder: (data: any, actions: any) => any; onApprove: (data: any, actions: any) => Promise<void>; onError: (err: any) => void; }) => { (): any; new(): any; render: { (arg0: any): void; new(): any; }; }; };
 
 @Component({
   selector: 'app-checkout',
@@ -9,7 +13,8 @@ import { Produto } from '../model/Produto';
   styleUrls: ['./checkout.component.css']
 })
 export class CheckoutComponent implements OnInit {
-
+  @ViewChild('paypal', { static: true }) paypalElement!: ElementRef;
+  paidFor = false
   produtoId: number
   produto: Produto = new Produto()
 
@@ -22,7 +27,33 @@ export class CheckoutComponent implements OnInit {
 
     this.produtoId = this.route.snapshot.params["id"]
     this.findByIdProduto(this.produtoId)
+
+
+    // paypal
+paypal.Buttons({
+  createOrder: (data: any, actions: any) => {
+    return actions.order.create({
+      purchase_units: [{
+        description: 'Experiências Compartilhai',
+        amount: {
+          currency_code: 'BRL',
+          value: this.produto.preco
+        }
+      }]
+    })
+  },
+  onApprove: async (data: any, actions: any) => {
+    const order = await actions.order.capture()
+    this.paidFor = true
+    console.log(order)
+  },
+  onError: (err: any) => {
+    console.log(err)
   }
+})
+  .render(this.paypalElement.nativeElement)
+
+}
 
   findByIdProduto(id: number) {
     this.produtoService.getByIdProduto(id).subscribe((resp: Produto)=>{
