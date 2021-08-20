e2c8e42f51e37a7900a4d12dabaa6362b1a1e5b0
JuCSMartin <julianacmsantos@gmail.com>
2021-08-20 18:30:08 -0300

    Validação do formulário de cadastro

--- src/app/cadastrar/cadastrar.component.ts
+++ src/app/cadastrar/cadastrar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { User } from '../model/User';
 import { AlertasService } from '../service/alertas.service';
@@ -15,11 +16,29 @@ export class CadastrarComponent implements OnInit {
   confirmarSenha: string
   tipoUsuario: string
 
+  meuFormGroup: FormGroup;
+
   constructor(
     private authService: AuthService,
     private router: Router,
-    private alertas: AlertasService
-  ) { }
+    private alertas: AlertasService,
+    private formBuilder: FormBuilder,
+  ) {
+    this.meuFormGroup = this.formBuilder.group({
+      nome: ['', Validators.required],
+      email: ['', [
+        Validators.required,
+        Validators.email
+      ]],
+      foto: ['', Validators.required],
+      senha: ['', [
+        Validators.required,
+        Validators.minLength(6)]],
+      confirmSenha: ['', Validators.required],
+      dataNasc: ['', Validators.required],
+      tipoUsuario: ['', Validators.required]
+    })
+   }
 
   ngOnInit() {
     window.scroll(0,0)
